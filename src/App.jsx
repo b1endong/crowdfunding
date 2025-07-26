c267b459a00aa8c094fe4d4474cdead31a0941fe
b1endong <tranhoanghai260805@gmail.com>
2025-07-26 14:40:38 +0700

    feature: Add Header and Connect Wallet

--- src/App.jsx
+++ src/App.jsx
@@ -1,7 +1,36 @@
-import "./App.css";
+import {createAppKit} from "@reown/appkit/react";
+import {EthersAdapter} from "@reown/appkit-adapter-ethers";
+import {arbitrum, mainnet, sepolia} from "@reown/appkit/networks";
+
+import Header from ".//components/Header";
+
+const projectId = "72404a238daa32ae2ccbebec83f33446";
+
+const networks = [arbitrum, mainnet, sepolia];
+
+const metadata = {
+    name: "Crowdfunding Dapp",
+    description: "A decentralized crowdfunding application",
+    url: "https://mywebsite.com", // origin must match your domain & subdomain
+    icons: ["https://avatars.mywebsite.com/"],
+};
+
+createAppKit({
+    adapters: [new EthersAdapter()],
+    networks,
+    metadata,
+    projectId,
+    features: {
+        analytics: true, // Optional - defaults to your Cloud configuration
+    },
+});
 
 function App() {
-    return <div>Hello</div>;
+    return (
+        <div>
+            <Header />
+        </div>
+    );
 }
 
 export default App;
